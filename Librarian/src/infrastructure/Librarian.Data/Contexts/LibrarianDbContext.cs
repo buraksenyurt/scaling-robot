beb248c9a9cc1f4b12038feb5919f83f74100598
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-21 18:03:12 +0300

    DbContext sınıfının eklenmesi.

---
+++ Librarian/src/infrastructure/Librarian.Data/Contexts/LibrarianDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Librarian.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Librarian.Data.Contexts
+{
+    public class LibrarianDbContext
+        : DbContext
+    {
+        public LibrarianDbContext(DbContextOptions<LibrarianDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Librarian/src/presentation/Librarian.WebApi/Startup.cs
+++ Librarian/src/presentation/Librarian.WebApi/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Librarian.Data.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Librarian.WebApi
@@ -26,7 +21,7 @@ namespace Librarian.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<LibrarianDbContext>(options => options.UseSqlite("Data Source=LibrarianDatabase.sqlite3"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
