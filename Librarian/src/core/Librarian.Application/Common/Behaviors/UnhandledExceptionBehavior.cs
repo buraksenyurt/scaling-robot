4e1f8d6f5d00968e49740056bd9dd2c5e6cd0428
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-22 13:02:12 +0300

    MediatR için exception yakalayıcı davranış eklendi.

---
+++ Librarian/src/core/Librarian.Application/Common/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Librarian.Application.Common.Behaviors
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger _logger;
+        public UnhandledExceptionBehavior(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception excp)
+            {
+                _logger.LogError(excp, "Talep geldi ama bir exception oluştu");
+                throw;
+            }
+        }
+    }
+}
