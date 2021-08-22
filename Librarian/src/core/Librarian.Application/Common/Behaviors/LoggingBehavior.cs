172268bc10bcdb1e129d963a81833677b032fe75
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-22 13:01:54 +0300

    MediatR için loglayıcı davranış eklendi.

---
+++ Librarian/src/core/Librarian.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,22 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Librarian.Application.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest>
+        : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+        public LoggingBehavior(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation($"Talep geldi {request}", requestName);
+        }
+    }
+}
