55b80753bf06a9e9351a65e501cf22ba7b78677d
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-22 13:17:30 +0300

    Doğrulama davranışının kullanacağı Exception sınıfı eklendi.

---
+++ Librarian/src/core/Librarian.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Librarian.Application.Common.Exceptions
+{
+    public class ValidationException
+        :Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
+        public ValidationException()
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        public ValidationException(IEnumerable<ValidationFailure> errors)
+            :this()
+        {
+            var errorGroups= errors
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage);
+
+            foreach (var e in errorGroups)
+            {
+                Errors.Add(e.Key, e.ToArray());
+            }
+        }
+
+    }
+}
