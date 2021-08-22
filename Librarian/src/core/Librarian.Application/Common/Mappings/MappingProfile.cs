9515873d9a6f4c3f30bdeabc8e56d92f50840d51
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-22 14:43:50 +0300

    AutoMapper eklendi

---
+++ Librarian/src/core/Librarian.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Librarian.Application.Common.Mappings;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Travel.Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var types = assembly.GetExportedTypes()
+              .Where(
+                        t => t.GetInterfaces()
+                        .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>))
+                    )
+              .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping") ?? type.GetInterface("IMapFrom`1").GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
