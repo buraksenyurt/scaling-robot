582515a60783d3fd33e93ccf5f4c023e69d0ac39
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-24 21:54:03 +0300

    Dependency ayarlarını üstlenen sınıf eklendi. Bi de bi paketi unutmuşum be yaa :S

---
+++ Librarian/src/core/Librarian.Application/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Librarian.Application.Common.Behaviors;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Librarian.Application
+{
+    /*
+     * Uygulama katmanının içindeki servisleri kullanan tarafa(örneğin Web API) bildirmek için kullandığımız bir sınıf olarak düşünebiliriz.
+     * IServiceCollection zaten .Net'in dahili DI servislerine ulaşmakta önemli bir aracı.
+     * AutoMapper, MediatR, Validation ve Behavior'ları servisler koleksiyonuna ekleyen bir metot.
+     * 
+     * Bu sınıfı büyük ihtimalle Web API projesinin Startup'ındaki ConfigureServices metodunda kullanacağız. Böylece Web API çalışma zamanına
+     * gerekli DI bağımlılıkları otomatik olarak yüklenmiş olacak.
+     */
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApp(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            services.AddTransient(typeof(IRequestPreProcessor<>), typeof(LoggingBehavior<>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+
+            return services;
+        }
+    }
+}
