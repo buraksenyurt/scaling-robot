d813d52dc4f61bf2b1e8c199ccbcd04597abcc56
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-08-28 16:29:26 +0300

    Cache kullanımı eklendi.

--- Librarian/src/core/Librarian.Application/Books/Queries/GetBooks/GetBooksQuery.cs
+++ Librarian/src/core/Librarian.Application/Books/Queries/GetBooks/GetBooksQuery.cs
@@ -4,7 +4,11 @@ using Librarian.Application.Common.Interfaces;
 using Librarian.Application.Dtos.Books;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,8 +21,8 @@ namespace Librarian.Application.Books.Queries.GetBooks
      * 
      */
     public class GetBooksQuery
-        :IRequest<BooksViewModel>
-    {        
+        : IRequest<BooksViewModel>
+    {
     }
 
     public class GetBooksQueryHandler
@@ -26,23 +30,47 @@ namespace Librarian.Application.Books.Queries.GetBooks
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
-        public GetBooksQueryHandler(IApplicationDbContext context,IMapper mapper)
+        private readonly IDistributedCache _distributedCache; //Redis Cache servisini Constructor Injection ile içeriye alıyoruz
+        public GetBooksQueryHandler(IApplicationDbContext context, IMapper mapper, IDistributedCache distributedCache)
         {
             _context = context;
             _mapper = mapper;
+            _distributedCache = distributedCache;
         }
         public async Task<BooksViewModel> Handle(GetBooksQuery request, CancellationToken cancellationToken)
         {
-            var books = new BooksViewModel
+            const string cacheKey = "GetBooks";
+            BooksViewModel books;
+            string serializedBookList;
+
+            // Öncelikle cache'de GetBooks key değeri ile duran bir içerik var mı kontrol ediyoruz
+            var redisData = await _distributedCache.GetAsync(cacheKey, cancellationToken);
+
+            if (redisData == null) // İçerik yoksa,
             {
-                BookList=await _context
+                books = new BooksViewModel
+                {
+                    BookList = await _context
                     .Books
                     .ProjectTo<BookDto>(_mapper.ConfigurationProvider)
-                    .OrderBy(b=>b.Title)
+                    .OrderBy(b => b.Title)
                     .ToListAsync(cancellationToken)
-            };
+                }; // normal EF üstünden repository'ye gidip veriyi çekiyor
 
-            return books;            
+                serializedBookList = JsonConvert.SerializeObject(books); // onu JSON olarak serileştiriyor
+                redisData = Encoding.UTF8.GetBytes(serializedBookList); // UTF8 formatında encode edip
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)) // 10 dakika süreyle
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(1)); // ve son 1 dakika içerisinde yeni bir istek gelmemişse
+                await _distributedCache.SetAsync(cacheKey, redisData, options, cancellationToken); // cache'e alıyoruz
+
+                return books;
+            }
+
+            serializedBookList = Encoding.UTF8.GetString(redisData); // içerik cache üstünde varsa onu UTF8 encode ettirip
+            books = JsonConvert.DeserializeObject<BooksViewModel>(serializedBookList); // JSON'dan ters serileştirerek model nesnesine alarak geriye döndürüyoruz
+
+            return books;
         }
     }
 }
