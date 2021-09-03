eed10b9c6ce086f9087311298f7682acb39c2d5c
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-03 22:26:10 +0300

    DI sınıfı eklendi.

---
+++ Librarian/src/infrastructure/Librarian.Identity/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Librarian.Application.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Librarian.Identity
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<AuthenticationSettings>(config.GetSection(nameof(AuthenticationSettings)));
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
