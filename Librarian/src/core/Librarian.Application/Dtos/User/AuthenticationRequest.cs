ebbceee813518bd23d6b9cea676da323977e98ac
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-03 21:53:27 +0300

    Auth için request/response sınıfları

---
+++ Librarian/src/core/Librarian.Application/Dtos/User/AuthenticationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Librarian.Application.Dtos.User
+{
+    /*
+     * Authentication aşamasında gelen talebin içinde Username ve Password bilgisinin taşınmasını sağlayan sınıfımız
+     */
+    public class AuthenticationRequest
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Librarian/src/core/Librarian.Application/Dtos/User/AuthenticationResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace Librarian.Application.Dtos.User
+{
+    /*
+     * Doğrulama başarılı olduğunda dönen response içeriğini temsil eden sınıf.
+     * 
+     * Aslında Claim sete ait bilgileri taşıdığını ifade edebiliriz.
+     * Önemli detaylardan birisi Password alanının olmayışı ama üretilecek JWT token için Token isimli bir özellik kullanışıdır.
+     */
+    public class AuthenticationResponse
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public AuthenticationResponse(Domain.Entities.User user, string token)
+        {
+            UserId = user.UserId;
+            Name = user.Name;
+            LastName = user.LastName;
+            Username = user.Username;
+            Email = user.Email;
+            Token = token;
+        }
+    }
+}
