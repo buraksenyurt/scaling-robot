3368feffd396933264c45fd1c88da05eb67892b3
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-03 21:56:01 +0300

    UserService sözleşmesi eklendi.

---
+++ Librarian/src/core/Librarian.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Librarian.Application.Dtos.User;
+using Librarian.Domain.Entities;
+
+namespace Librarian.Application.Common.Interfaces
+{
+    /*
+     * Kullanıcı servisi response içeriği ile gelen bilgilere göre doğrulama işini üstlenen
+     * ve id üstünden kullanıcı bilgisi döndüren fonksiyonları tarifleyen bir sözleşme sunuyor.
+     */
+    public interface IUserService
+    {
+        AuthenticationResponse Authenticate(AuthenticationRequest model);
+        User GetById(int userId);
+    }
+}
---
+++ Librarian/src/core/Librarian.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Librarian.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
