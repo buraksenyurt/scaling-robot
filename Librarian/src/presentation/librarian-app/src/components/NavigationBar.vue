8aa5749e64660bc3230640c9d8771d8e0ed740a5
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-07 19:17:40 +0300

    Navigasyon bileşeni eklendi.

---
+++ Librarian/src/presentation/librarian-app/src/components/NavigationBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar app color="primary" light>
+    <div>
+      <v-btn color="primary" outlined :to="{ path: '/' }">
+        <span class="menu">Lobi</span>
+      </v-btn>
+
+      <router-link to="/about">
+        <v-btn color="primary" outlined>
+          <span class="menu">Bilgi</span>
+        </v-btn>
+      </router-link>
+
+      <v-btn color="primary" outlined :to="{ path: '/dashboard' }">
+        <span class="menu">Envanter Yönetimi</span>
+      </v-btn>
+      <v-btn color="primary" outlined :to="{ path: '/login' }">
+        <span class="menu">Giriş</span>
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+};
+</script>
+
+<style scoped>
+.menu {
+  color: white;
+  text-decoration: none;
+}
+</style>
