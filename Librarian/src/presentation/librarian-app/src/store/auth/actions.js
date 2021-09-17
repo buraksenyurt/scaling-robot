b3c00ac309d6adb2f2a8fd325ff8893d33e15a35
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-17 21:11:36 +0300

    Authorization hazırlıkları için store güncellemesi.

---
+++ Librarian/src/presentation/librarian-app/src/store/auth/actions.js
@@ -0,0 +1,13 @@
+import * as types from "./action-types";
+import {
+    loginUser
+} from "@/auth/authService";
+
+export async function loginUserAction({ commit }, payload) {
+    try {
+        const { data } = await loginUser(payload);
+        commit(types.LOGIN_USER, data.token);
+    } catch (e) {
+        console.log(e);
+    }
+}
--- Librarian/src/presentation/librarian-app/src/store/index.js
+++ Librarian/src/presentation/librarian-app/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import createLogger from "vuex/dist/logger";
 import bookModule from "./book";
+import authModule from "./auth";
 
 Vue.use(Vuex);
 
@@ -10,7 +11,8 @@ const plugins = debug ? [createLogger({})] : [];
 
 export default new Vuex.Store({
   modules: {
-    bookModule
+    bookModule,
+    authModule
   },
   plugins
 });
