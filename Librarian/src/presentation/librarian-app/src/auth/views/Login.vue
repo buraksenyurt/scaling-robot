66022a7b6e9905013ef756d463780b3fb858f4ce
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-17 21:51:24 +0300

    Login ve yeni kullanıcı kaydı için bileşen eklendi

---
+++ Librarian/src/presentation/librarian-app/src/auth/views/Login.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-app>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+      min-width="360px"
+      @click:outside="navigateHome"
+    >
+      <div>
+        <v-tabs
+          show-arrows
+          background-color="pink accent-4"
+          icons-and-text
+          dark
+          grow
+        >
+          <v-tabs-slider color="pink darken-4"></v-tabs-slider>
+          <v-tab>
+            <v-icon large>mdi-login</v-icon>
+            <div>Giriş</div>
+          </v-tab>
+          <v-tab>
+            <v-icon large>mdi-account-box-outline</v-icon>
+            <div>Kayıt</div>
+          </v-tab>
+          <v-tab-item>
+            <v-card class="px-4">
+              <v-card-text>
+                <form @submit.prevent="onSubmit">
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="E-mail"
+                        v-model="login.email"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        type="password"
+                        label="Şifre"
+                        hint="Şöyle güçlü bir şeyler gir"
+                        counter
+                        v-model="login.password"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col class="d-flex" cols="12" sm="6" xsm="12"> </v-col>
+                    <v-spacer></v-spacer>
+                    <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                      <v-btn :disabled="false" color="primary" type="submit"
+                        >Dene</v-btn
+                      >
+                    </v-col>
+                  </v-row>
+                </form>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card class="px-4">
+              <v-card-text>
+                <div>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field
+                        label="Adın"
+                        maxlength="20"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field
+                        label="Soyadın"
+                        maxlength="20"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field
+                        label="Birde kullanıcı adın ;)"
+                        maxlength="20"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="E-Posta Adresin"
+                        required
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Şifre"
+                        hint="Şöyle kuvvetli bir şifre belirlesek ya."
+                        counter
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        block
+                        label="Şifre(Yeniden)"
+                        counter
+                      ></v-text-field>
+                    </v-col>
+                    <v-spacer></v-spacer>
+                    <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                      <v-btn :disabled="false" color="primary">Kaydol</v-btn>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </div>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import router from "@/router";
+export default {
+  name: "Login",
+  data: () => ({
+    dialog: true,
+    tab: null,
+    login: {
+      email: "",
+      password: "",
+    },
+  }),
+  methods: {
+    ...mapActions("authModule", ["loginUser"]),
+    onSubmit() {
+      this.loginUser(this.login).then(() => {
+        this.$router.push({ path: "/dashboard" });
+      });
+    },
+    navigateHome() {
+      router.push("/");
+    },
+  },
+};
+</script>
