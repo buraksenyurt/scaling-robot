e58752231e535065e00f36af5fb3ec97bea1a05d
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-04 22:12:11 +0300

    DbFixture için ek metotlar.

--- Librarian/tests/Librarian.IntegrationTests/DbFixture.cs
+++ Librarian/tests/Librarian.IntegrationTests/DbFixture.cs
@@ -1,5 +1,6 @@
 ﻿using Librarian.Data.Contexts;
 using Librarian.WebApi;
+using MediatR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,7 @@ using Moq;
 using Respawn;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Librarian.IntegrationTests
 {
@@ -44,12 +46,36 @@ namespace Librarian.IntegrationTests
             _scopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
             _checkpoint = new Checkpoint
             {
-                TablesToIgnore = new[] { "__EFMigrationsHistory" }
+                TablesToIgnore = new[] { "__EFMigrationsHistory" } // Respawn paketindeki Checkpoint nesnesini oluştururken bu tabloyu dikkate almamasını belirtmiş olduk
             };
             using var scope = _scopeFactory.CreateScope();
             var context = scope.ServiceProvider.GetService<LibrarianDbContext>(); //EF context servisini çektik
             context.Database.Migrate(); // Migration işlemi başlatıldı
         }
+        // Mediator'a talep göndermek için.
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope(); // Scope oluşturuluyor
+            var mediator = scope.ServiceProvider.GetService<IMediator>(); // O scope'tan IMediator implementasyonunu alıyoruz
+            return await mediator.Send(request); // Gelen servis talebini gönderiyoruz
+        }
+
+        // Yeni bir Entity nesnesini veritabanına eklemek için.
+        public static async Task AddAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope(); // Scope oluşturuluyor
+            var context = scope.ServiceProvider.GetService<LibrarianDbContext>(); // Oluşturulan Scope'dan EF servis nesnesi alınıyor
+            context.Add(entity); // Gelen entity veritabanına ekleniyor
+            await context.SaveChangesAsync(); // Kayıt
+        }
+
+        // Test veritabanını ilk konumuna almak için
+        // Respawn ile gelen Checkpoint nesnesi yardımıyla konfigurasyondaki veritabanı bağlantısı için bu işlem gerçeklenir.
+        public static async Task ResetState()
+        {
+            await _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
         public void Dispose()
         {
         }
