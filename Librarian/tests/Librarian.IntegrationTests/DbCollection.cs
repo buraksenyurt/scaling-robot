5c56f72a0a10b38d1309761e63a75f28944a1b18
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-10-04 22:20:54 +0300

    Entegrasyon testleri için ek geliştirmeler.

---
+++ Librarian/tests/Librarian.IntegrationTests/DbCollection.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Librarian.IntegrationTests
+{
+    /*
+     * Bu projedeki birim testlerin tamamının bu koleksiyon nesnesine eklenen DbFixture örneğini kullanacağını belirtmiş olur.
+     * Her test için DbFixture'u düşünmemize gerek kalmaz.
+     * Testler sonlandığında, DbFixture nesne yok edilme sürecine girer nitekim IDisposable arayüzünü uygulamaktadır.
+     */
+    [CollectionDefinition("DbCollection")]
+    public class DbCollection
+        :ICollectionFixture<DbFixture>
+    {
+    }
+}
